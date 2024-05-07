4fb302e976b83bdf84e7ff1ad53248000554e1d5
Deena08 <dhinakaranuma@gmail.com>
2024-05-07 15:44:52 +0530

    first commit create to do app using react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import TodoPage from './components/TodoPage/TodoPage';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+     <TodoPage/>
+    </div>
+  );
+}
+
+export default App;
